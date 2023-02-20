727aff9fc95ccdcfb2bc3f6ee08a0b3d73c32081
MatiWolFo <mat.aravenar@hmail.com>
2023-02-20 12:15:43 -0400

    fix bug

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   // raiz o root del proyecto
-  base: "https://matiwolfo.github.io/",
+  base: "https://matiwolfo.github.io/ghub-actions-demo/",
   plugins: [react()],
 })
